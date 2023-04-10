64c63ea16ad7d96c8227fba345b55922c29fdce3
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-10 16:48:32 -0700

    feat: init mongoose

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import mongoose from "mongoose";
+
+// mongo
+const CONNECTION_STRING = process.env.SHIPSHARE_WEB_KEY
+    || 'mongodb://localhost:27017/web-shipshare';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(express.json());
