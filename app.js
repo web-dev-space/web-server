632b1eff233fbcaa06319802a8c45a56c9adfab7
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-10 19:15:16 -0700

    feat: init cors

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 import express from 'express';
+import cors from 'cors'
 import mongoose from "mongoose";
 import {ParcelsController} from "./DBFunctions/parcels/parcels-controllers.js";
 
 
-// mongo
+// -------------Set Ups----------------
+// link to mongoDB
 const CONNECTION_STRING = process.env.SHIPSHARE_WEB_KEY
     || 'mongodb://localhost:27017/web-shipshare';
 mongoose.connect(CONNECTION_STRING);
 
 
-// express
-const app = express()
+// express & cors
+const app = express();
 app.use(express.json());
+app.use(cors());
 
 
-// -------------controllers----------------
+// -------------Controllers----------------
 ParcelsController(app);
 
 
