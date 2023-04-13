363d44e46c8d333362057ee1c53df81ebc0c3cf3
Zhongyi Lu <98251609+Zhongyi-Lu@users.noreply.github.com>
2023-04-13 00:53:36 -0700

    fix: `memoryStorage()` cannot be used in production environment (#7)
    * fix: `memoryStorage()` cannot be used in production environment
    
    * fix: typo

--- DBFunctions/cloudinary/image-upload-controllers.js
+++ DBFunctions/cloudinary/image-upload-controllers.js
@@ -1,33 +1,46 @@
-import multer from "multer";
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 import { v2 as cloudinary } from 'cloudinary';
 import { v4 as uuidv4 } from 'uuid';
+import Busboy from 'busboy';
 
 const ImageController = (app) => {
-    app.post('/upload_image', upload.single('image'), async (req, res) => {
+    app.post('/upload_image', async (req, res) => {
         try {
+            const busboy = Busboy({ headers: req.headers });
             const filename = uuidv4();
+            let bufferData = Buffer.from('');
 
-            // Upload image to cloudinary using a buffer
-            const result = await new Promise((resolve, reject) => {
-                const uploadStream = cloudinary.uploader.upload_stream(
-                    {
-                        resource_type: 'image',
-                        public_id: filename,
-                    },
-                    (error, result) => {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            resolve(result);
-                        }
+            busboy.on('file', (_, file) => {
+
+                file.on('data', (data) => {
+                    bufferData = Buffer.concat([bufferData, data]);
+                });
+
+                file.on('end', async () => {
+                    try {
+                        const result = await cloudinary.uploader.upload_stream(
+                            {
+                                resource_type: 'image',
+                                public_id: filename,
+                            },
+                            (error, result) => {
+                                if (error) {
+                                    console.log(error);
+                                    return res.status(400).json({ message: error?.message });
+                                } else {
+                                    return res.status(200).json({ success: true, imageUrl: result.secure_url });
+                                }
+                            }
+                        ).end(bufferData);
+                    } catch (err) {
+                        console.log(err);
+                        res.status(400).json({ message: err?.message });
                     }
-                );
+                });
 
-                uploadStream.end(req.file.buffer);
             });
-            res.status(200).json({ success: true, imageUrl: result.secure_url });
+
+            req.pipe(busboy);
+
         } catch (err) {
             console.log(err);
             res.status(400).json({ message: err?.message });
