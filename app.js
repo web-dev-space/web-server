2e4fc2f28769514b4ea9903f3cec051eea16ee6d
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-12 23:33:54 -0700

    feat: add ImageController

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ import express from 'express';
 import cors from 'cors'
 import session from "express-session";
 import mongoose from "mongoose";
-import {ParcelsController} from "./DBFunctions/parcels/parcels-controllers.js";
+import { ParcelsController } from "./DBFunctions/parcels/parcels-controllers.js";
 import { ShipGroupsController } from './DBFunctions/shipGroups/shipGroups-controllers.js';
 import { PostController } from './DBFunctions/posts/posts-controllers.js';
-import {UsersController} from "./DBFunctions/users/users-controllers.js";
-import {AuthController} from "./DBFunctions/users/auth-controller.js";
+import { UsersController } from "./DBFunctions/users/users-controllers.js";
+import { AuthController } from "./DBFunctions/users/auth-controller.js";
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import { v2 as cloudinary } from 'cloudinary';
+import ImageController from "./DBFunctions/cloudinary/image-upload-controllers.js";
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 // -------------Set Ups----------------
 // link to mongoDB
@@ -15,6 +20,15 @@ const CONNECTION_STRING = process.env.SHIPSHARE_WEB_KEY
     || 'mongodb://localhost:27017/web-shipshare';
 console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
+
+
+// Return "https" URLs by setting secure: true
+cloudinary.config({
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // express & cors
 const app = express();
@@ -52,6 +66,7 @@ ShipGroupsController(app);
 PostController(app);
 UsersController(app);
 AuthController(app);
+ImageController(app);
 
 
 app.listen(4000)
