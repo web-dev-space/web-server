2b4ee175ebaa6fddda61179220edc2fd2a9f051f
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-13 21:16:30 -0700

    fix: error handle

--- DBFunctions/users/users-controllers.js
+++ DBFunctions/users/users-controllers.js
@@ -9,10 +9,13 @@ export const UsersController = (app) => {
     app.delete('/users/:id', deleteUser);
     app.put('/users/:id', updateUser);
 }
-
 const findAllUsers = async (req, res) => {
-    const users = await usersDao.findAllUsers();
-    res.json(users);
+    try {
+        const users = await usersDao.findAllUsers();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while fetching all users." });
+    }
 }
 
 const findUserById = async (req, res) => {
@@ -22,7 +25,7 @@ const findUserById = async (req, res) => {
         res.json(users);
     }
     catch (error) {
-        res.json(error);
+        res.status(500).json({ message: `An error occurred while fetching the user with ID ${idToFind}.` });
     }
 }
 
@@ -32,59 +35,66 @@ const findUserByEmail = async (req, res, next) => {
         const users = await usersDao.findUserByEmail(emailToFind);
         res.json(users[0]);
     } catch (error) {
-        next(error);
+        res.status(500).json({ message: `An error occurred while fetching the user with email ${emailToFind}.` });
     }
 }
 // b. Delete -- return null when unsuccessful
 const deleteUser = async (req, res) => {
-    const idToDelete = req.params.id;
-    const user = await usersDao.findUserById(idToDelete);
-    if (!user) {
-        res.json(null);
-        return;
+    try {
+        const idToDelete = req.params.id;
+        const user = await usersDao.findUserById(idToDelete);
+        if (!user) {
+            res.status(404).json({ message: `User with ID ${idToDelete} not found.` });
+            return;
+        }
+        const role = user.role;
+        let status = null;
+        if (role === 'buyer') {
+            status = await usersDao.deleteBuyer(idToDelete);
+        }
+        else if (role === 'merchant') {
+            status = await usersDao.deleteMerchant(idToDelete);
+        }
+        else if (role === 'admin') {
+            status = await usersDao.deleteAdmin(idToDelete);
+        }
+        res.json(status);
+    } catch (error) {
+        res.status(500).json({ message: `An error occurred while deleting the user with ID ${req.params.id}.` });
     }
-    const role = user.role;
-    let status = null;
-    if (role === 'buyer') {
-        status = await usersDao.deleteBuyer(idToDelete);
-    }
-    else if (role === 'merchant') {
-        status = await usersDao.deleteMerchant(idToDelete);
-    }
-    else if (role === 'admin') {
-        status = await usersDao.deleteAdmin(idToDelete);
-    }
-    res.json(status);
 }
 
 
 // c. Update -- return null when unsuccessful
 const updateUser = async (req, res) => {
-    const idToUpdate = req.params.id;
-    const updatedUser = req.body;
+    try {
+        const idToUpdate = req.params.id;
+        const updatedUser = req.body;
 
-    // Get original user object
-    const user = await usersDao.findUserById(idToUpdate);
-    if (!user) {
-        res.json(null);
-        return;
-    }
-    const role = user.role;
-    let status = null;
+        // Get original user object
+        const user = await usersDao.findUserById(idToUpdate);
+        if (!user) {
+            res.status(404).json({ message: `User with ID ${idToUpdate} not found.` });
+            return;
+        }
+        const role = user.role;
+        let status = null;
 
-    // Update user object - based on role
-    if (role === 'buyer') {
-        status = await usersDao.updateBuyer(idToUpdate, updatedUser)
-            .then(() => usersDao.findBuyerById(idToUpdate));
+        // Update user object - based on role
+        if (role === 'buyer') {
+            status = await usersDao.updateBuyer(idToUpdate, updatedUser)
+                .then(() => usersDao.findBuyerById(idToUpdate));
+        }
+        else if (role === 'merchant') {
+            status = await usersDao.updateMerchant(idToUpdate, updatedUser)
+                .then(() => usersDao.findMerchantById(idToUpdate));
+        }
+        else if (role === 'admin') {
+            status = await usersDao.updateAdmin(idToUpdate, updatedUser)
+                .then(() => usersDao.findAdminById(idToUpdate));
+        }
+        res.json(status);
+    } catch (error) {
+        res.status(500).json({ message: `An error occurred while updating the user with ID ${req.params.id}.` });
     }
-    else if (role === 'merchant') {
-        status = await usersDao.updateMerchant(idToUpdate, updatedUser)
-            .then(() => usersDao.findMerchantById(idToUpdate));
-    }
-    else if (role === 'admin') {
-        status = await usersDao.updateAdmin(idToUpdate, updatedUser)
-            .then(() => usersDao.findAdminById(idToUpdate));
-    }
-
-    res.json(status);
 }
