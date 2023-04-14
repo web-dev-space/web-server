7e7ea58fba9b7e2828233dc42bdb0b1997cfa7ce
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-14 14:58:26 -0700

    feat: count shipGroup number

--- DBFunctions/shipGroups/shipGroups-dao.js
+++ DBFunctions/shipGroups/shipGroups-dao.js
@@ -1,23 +1,26 @@
-import shipGroupsModel from './shipGroups-model.js';
+import shipGroupsModel from "./shipGroups-model.js";
 
-export const findAllShipGroups = async () =>
-    await shipGroupsModel.find();
+export const findAllShipGroups = async () => await shipGroupsModel.find();
 
 export const findShipGroupById = async (id) =>
-    await shipGroupsModel.findById(id);
+  await shipGroupsModel.findById(id);
 
 export const findShipGroupByTrackingNumber = async (trackingNumber) =>
-    await shipGroupsModel.findOne({ trackingNumber });
+  await shipGroupsModel.findOne({ trackingNumber });
 
 export const createShipGroup = async (newShipGroup) =>
-    await shipGroupsModel.create(newShipGroup);
+  await shipGroupsModel.create(newShipGroup);
 
 export const deleteShipGroup = async (id) =>
-    await shipGroupsModel.findByIdAndDelete({ _id: id });
+  await shipGroupsModel.findByIdAndDelete({ _id: id });
 
 export const updateShipGroup = async (id, newShipGroup) =>
-    await shipGroupsModel.findByIdAndUpdate(
-        { _id: id },
-        { $set: newShipGroup },
-        { new: true }
-    )
+  await shipGroupsModel.findByIdAndUpdate(
+    { _id: id },
+    { $set: newShipGroup },
+    { new: true }
+  );
+
+export const countAllShipGroups = async () => {
+  return { totalShipGroupsNumber: await shipGroupsModel.countDocuments({}) };
+};
