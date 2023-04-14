89e3f922c90926b534a1d569f965a50d358955c3
CuichanWu <wu.cu@northeastern.edu>
2023-04-14 13:33:24 -0700

    feat: update post schema

--- DBFunctions/posts/posts-schema.js
+++ DBFunctions/posts/posts-schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const schema = mongoose.Schema(
   {
+    userId: { type: String, required: true },
     title: { type: String, required: true },
     post: { type: String, required: true },
     image: { type: String, required: true },
