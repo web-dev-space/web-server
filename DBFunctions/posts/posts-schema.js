adc308f455fd71db189c85d386e3dc90bc4a1afb
CuichanWu <wu.cu@northeastern.edu>
2023-04-15 16:39:58 -0700

    feat: update post schema

--- DBFunctions/posts/posts-schema.js
+++ DBFunctions/posts/posts-schema.js
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 
-const schema = mongoose.Schema(
+const commentSchema = mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+const postSchema = mongoose.Schema(
   {
     userId: { type: String, required: true },
     title: { type: String, required: true },
     post: { type: String, required: true },
     image: { type: String, required: true },
     comments: {
-      type: [{ user: String, date: Date, content: String }],
+      type: [commentSchema],
       required: true,
+      default: [],
     },
     viewsAmount: { type: Number, required: true },
     created: { type: Date, required: true },
@@ -16,4 +33,4 @@ const schema = mongoose.Schema(
   { collection: "posts" }
 );
 
-export default schema;
+export default postSchema;
