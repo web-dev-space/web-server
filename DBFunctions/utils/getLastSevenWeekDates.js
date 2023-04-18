f27f52090f060ba7d21eb4123dbf63e06df9c47a
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-18 16:42:05 -0700

    fix: date

--- DBFunctions/utils/getLastSevenWeekDates.js
+++ DBFunctions/utils/getLastSevenWeekDates.js
@@ -5,7 +5,7 @@ export default function getLastSevenWeekDates() {
   for (let i = 6; i >= 0; i--) {
     // Calculate past week date
     const pastWeekDate = new Date(today);
-    pastWeekDate.setDate(today.getDate() - 7 * i);
+    pastWeekDate.setDate(today.getDate() - 1 - 7 * i);
 
     // Format the date as 'M/D'
     const formattedDate = `${pastWeekDate.getMonth() + 1}/${pastWeekDate.getDate()}`;
