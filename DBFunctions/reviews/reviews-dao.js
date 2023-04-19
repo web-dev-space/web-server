ed9628edee695fb863bbc9460066165b241d3269
Zhongyi Lu <98251609+Zhongyi-Lu@users.noreply.github.com>
2023-04-19 13:06:47 -0700

    Feat: review api (#25)
    * feat: add `findReviewsForProject`
    
    * feat: add cache for product search

--- DBFunctions/reviews/reviews-dao.js
+++ DBFunctions/reviews/reviews-dao.js
@@ -10,3 +10,6 @@ export const deleteReview = (id) => reviewsModel.findByIdAndDelete({ _id: id });
 
 export const updateReview = (id, newReview) =>
   reviewsModel.findByIdAndUpdate({ _id: id }, { $set: newReview });
+
+export const findReviewsForProject = (projectId) =>
+  reviewsModel.find({ asin: projectId }).sort({ date: -1 }).exec();
