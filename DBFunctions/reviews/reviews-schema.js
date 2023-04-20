60cb7e455627afbec73da0de37e606f912f39d06
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-20 16:01:59 -0700

    feat: update schema for reviews

--- DBFunctions/reviews/reviews-schema.js
+++ DBFunctions/reviews/reviews-schema.js
@@ -6,6 +6,9 @@ const schema = mongoose.Schema(
     user: { type: String, required: true },
     date: { type: Date, required: true },
     content: { type: String, required: true },
+    productName: { type: String, required: true },
+    productLink: { type: String, required: true },
+    productImage: { type: String, required: true },
   },
   { collection: "reviews" }
 );
